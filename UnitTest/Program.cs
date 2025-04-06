cd5a2ae4f834e2166392a83e16f038f4ee289e5b
karimmoustamid <karimmoustamid@KARIMMOUSTAFA23>
2025-04-06 15:29:54 -0600

    twdbhw
    Add unit tests for WorldsDumbestFunction
    
    Introduce `WorldsDumbestFunctionTests` with a method
    `ReturnsSomethingIfZero_WhenInputIsZero_ReturnsSomething`
    to validate the behavior of `ReturnsSomethingIfZero`
    when the input is zero. Update `Program.cs` to call
    the new test method.

--- UnitTest/Program.cs
+++ UnitTest/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using UnitTest.Tests;
+
 Console.WriteLine("Hello, World!");
+WorldsDumbestFunctionTests.ReturnsSomethingIfZero_WhenInputIsZero_ReturnsSomething();
---
+++ UnitTest/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Tests
+{
+    public static class WorldsDumbestFunctionTests
+    {
+        // Naming Convention: TestMethodName_StateUnderTest_ExpectedBehavior
+        public static void ReturnsSomethingIfZero_WhenInputIsZero_ReturnsSomething()
+        {
+            try
+            {
+                // Arrange - Initialize variables, set up necessary objects, and prepare the environment for the test
+                int num = 0;
+                WorldsDumbestFunction worldsDumbestFunction = new WorldsDumbestFunction();
+
+                // Act - execute the method or functionality being tested
+                string result = worldsDumbestFunction.ReturnsSomethingIfZero(num);
+
+
+                // Assert - whatever is returned from the method, check if it meets the expected outcome
+
+                if (result == "Something")
+                {
+                    Console.WriteLine("Test Passed: " + result);
+                }
+                else
+                {
+                    Console.WriteLine("Test Failed: Expected 'Something', but got " + result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
