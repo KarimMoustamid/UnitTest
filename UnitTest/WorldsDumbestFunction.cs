a49b19b46d467c9f0a175a3fd8ba4d5156590795
karimmoustamid <karimmoustamid@KARIMMOUSTAFA23>
2025-04-06 14:57:27 -0600

    Add project files.

---
+++ UnitTest/WorldsDumbestFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class WorldsDumbestFunction
+    {
+        public string ReturnsSomethingIfZero(int num)
+        {
+            if (num == 0)
+            {
+                return "Something";
+            }
+            else
+            {
+                return "Nothing";
+            }
+        }
+    }
+}
